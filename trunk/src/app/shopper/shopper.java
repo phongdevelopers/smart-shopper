0cc1ab67156bcd4322399f7d6873ee6bef8868aa
karthikt.holmes <karthikt.holmes@7f2669b8-b017-3283-2efb-3acce199a7a2>
2010-07-25 15:42:03 +0000

    tags

--- trunk/src/app/shopper/shopper.java
+++ trunk/src/app/shopper/shopper.java
@@ -1,26 +1,33 @@
 package app.shopper;
 
-import android.app.Activity;
 import android.app.Dialog;
+import android.app.TabActivity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.content.DialogInterface.OnCancelListener;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ScrollView;
+import android.widget.TabHost;
 import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.TabHost.OnTabChangeListener;
+import android.widget.TabHost.TabContentFactory;
 
-public class shopper extends Activity implements OnClickListener {
+public class shopper extends TabActivity implements OnClickListener, OnTabChangeListener, TabContentFactory, OnCancelListener {
     static Context con;
     ItemList itemList ;
     int layout;
@@ -31,49 +38,109 @@ public class shopper extends Activity implements OnClickListener {
     private static final int DELETE_ID = 11;
     
 	private static final int DIALOG_ABOUT = 21;
-	private static final int DIALOG_HELP = 22;
+	private static final int DIALOG_HELP = 22;	
+	private static final int DIALOG_NEW = 23;
 	
 	private static final int MENU_ABOUT = 31;
 	private static final int MENU_HELP = 32;
+	private static final String shoppingList = "shoppingList";
+	private static final String itemlist = "itemList";
+	
 	InputMethodManager imm;    
+	TabHost tabHost;
+	
+	String hint = "";
+	
+	public void setUpTabs(){
+	    setContentView(R.layout.tabmain);
+
+	    tabHost = getTabHost();  // The activity TabHost
+	    TabHost.TabSpec spec ;  // Resusable TabSpec for each tab
+
+
+	    // Initialize a TabSpec for each tab and add it to the TabHost
+	    spec = tabHost.newTabSpec(itemlist);
+	    spec.setIndicator("Item List");
+	    spec.setContent(this);//R.layout.main);
+	    tabHost.addTab(spec);
+
+	    // Do the same for the other tabs
+	    spec = tabHost.newTabSpec(shoppingList).setIndicator("Shopping List").setContent(this);//R.layout.shoppinglist);
+	    tabHost.addTab(spec);
+
+	    tabHost.setOnTabChangedListener(this);
+
+
+	}
     
+	public void showItemList(){
+		showShoppingList();
+		tabHost.setCurrentTabByTag(itemlist);
+	}
 	public void displayItemList(){
 		//hiding keyboard
+		debug("Item List");
+		/*
 		if(layout == R.layout.newitem)
-			imm.hideSoftInputFromWindow(this.findViewById(R.id.EditText01).getWindowToken(), 0);
-		
-		setContentViewCustom(R.layout.main);	
+			imm.hideSoftInputFromWindow(this.findViewById(R.id.EditText01).getWindowToken(), 0);*/
+		/*//tabHost.setCurrentTabByTag("items");
+		setContentViewCustom(R.layout.main);	*/
 			ScrollView sc=(ScrollView) this.findViewById(R.id.ScrollView01);
-			sc.addView(itemList.display(false));	
-			registerForContextMenu(itemList.display(false));
-			this.findViewById(R.id.Button01).requestFocus();
-			this.findViewById(R.id.Button01).setOnClickListener(this);
-			this.findViewById(R.id.Button02).setOnClickListener(this);
+			sc.removeAllViews();
+			View v= itemList.display(false);
+			sc.addView(v);	
+			layout = R.layout.main;
+			//this.findViewById(R.id.Button01).requestFocus();//Fix For TrackBall
 	}
+	
+	
+	public void showShoppingList(){
+		tabHost.setCurrentTabByTag(shoppingList);
+	}	
 	
 	public void displayShoppingList(){
-		setContentViewCustom(R.layout.shoppinglist);
-			ScrollView sc=(ScrollView) this.findViewById(R.id.ScrollView01);
+		/*setContentViewCustom(R.layout.shoppinglist);
+
+		//tabHost.setCurrentTabByTag("list");
+*/
+		layout = R.layout.shoppinglist;
+		debug("Shopping List");
+		ScrollView sc=(ScrollView) this.findViewById(R.id.ScrollView02);
+			sc.removeAllViews();
 			sc.addView(itemList.display(true));		
-			this.findViewById(R.id.Button01).requestFocus();
-			this.findViewById(R.id.Button01).setOnClickListener(this);
+			//this.findViewById(R.id.Button01).requestFocus();//Fix For TrackBall
 	}
 	
-	public void displayNewItem(){
-		setContentViewCustom(R.layout.newitem);
-			this.findViewById(R.id.EditText01).requestFocus();
-			
-			//pop up keyboard
-			imm.showSoftInput(this.findViewById(R.id.EditText01), InputMethodManager.SHOW_FORCED);
-			
-			this.findViewById(R.id.Button01).setOnClickListener(this);
-			this.findViewById(R.id.Button02).setOnClickListener(this);
-			this.findViewById(R.id.Button03).setOnClickListener(this);
+	void showNewItem(){
+		showDialog(DIALOG_NEW);
 	}
+	
+	public void displayNewItem(Dialog dialog){		
+		debug("Dialog");
+		dialog.setContentView(R.layout.newitem);
+    	dialog.setTitle(R.string.add_item_title);
+    	layout=R.layout.newitem;
+    	if(hint!="") 
+    		((EditText) dialog.findViewById(R.id.EditText01)).setHint(hint);
+    	
 
-	public void setContentViewCustom(int id){
-		setContentView(id);
-		layout = id;
+    	/*dialog.findViewById(R.id.EditText01).setOnFocusChangeListener(new View.OnFocusChangeListener() {
+    	    @Override
+    	    public void onFocusChange(View v, boolean hasFocus) {
+    	       // if (hasFocus) {
+    	        	imm.showSoftInput(v, InputMethodManager.SHOW_IMPLICIT);	
+    	       // }
+    	    }
+    	});*/
+    	//dialog.findViewById(R.id.EditText01).requestFocus();	
+        imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT,
+                InputMethodManager.HIDE_IMPLICIT_ONLY); 
+		//imm.showSoftInput(dialog.findViewById(R.id.EditText01), InputMethodManager.SHOW_FORCED);			
+		dialog.findViewById(R.id.Button03).setOnClickListener(this);
+		dialog.findViewById(R.id.Button01).setOnClickListener(this);
+		dialog.findViewById(R.id.Button02).setOnClickListener(this);
+		dialog.setOnCancelListener(this);
+		hint = "";
 	}
 
 	public static void debug(String debug){
@@ -85,16 +152,19 @@ public class shopper extends Activity implements OnClickListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
+		
 	    Context con = this.getApplicationContext();
 	    shopper.con = con;
 	    itemList =new ItemList(this);
 		SharedPreferences settings = getPreferences(Context.MODE_PRIVATE);
 		itemList.loadItemList(settings);
 		imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+		
+		setUpTabs();
 		if(itemList.itemList.isEmpty()||((ViewGroup) itemList.display(true)).getChildCount()==0)
-			displayItemList();
+			showItemList();
 		else 
-			displayShoppingList();
+			showShoppingList();
 	}
 
 	@Override
@@ -109,17 +179,25 @@ public class shopper extends Activity implements OnClickListener {
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
 		switch(layout){
-		case R.layout.main:displayItemList();break;
-		case R.layout.newitem:displayNewItem();break;
+		case R.layout.main:showItemList();break;
+		case R.layout.newitem:showNewItem();break;//displayNewItem();break;
 		case R.layout.shoppinglist:displayShoppingList();break;
 		}
 	}
+@Override
+	public void onCancel(DialogInterface dialog) {
+		// TODO Auto-generated method stub
+		onBackPressed();
+	}
 
+	//TODO bug pressback on new item then rotate phone
 	@Override
 	public void onBackPressed() {
-		if(layout!=R.layout.main)
-			displayItemList();
-		else
+		if(layout!=R.layout.main){
+			if(layout == R.layout.newitem) dismissDialog(DIALOG_NEW);
+			showItemList();
+			debug("BackPressed");
+		}else
 			super.onBackPressed();
 	}
 
@@ -132,7 +210,8 @@ public class shopper extends Activity implements OnClickListener {
 		case R.layout.main:
 			switch(button){
 			case R.id.Button01:displayShoppingList();break;
-			case R.id.Button02:displayNewItem();break;
+			case R.id.Button02:showNewItem();break;
+				//displayNewItem();break;
 			}
 			break;
 			
@@ -140,34 +219,31 @@ public class shopper extends Activity implements OnClickListener {
 		case R.layout.newitem:
 			//processing
 			if(button == R.id.Button01||button == R.id.Button03){
-				name = ((EditText) this.findViewById(R.id.EditText01)).getText().toString();
+				name = ((EditText) v.getRootView().findViewById(R.id.EditText01)).getText().toString();
 				if(name.length()>=1)
-					itemList.addItem(new Item(name,((CheckBox) this.findViewById(R.id.CheckBox01)).isChecked()));
+					itemList.addItem(new Item(name,((CheckBox) v.getRootView().findViewById(R.id.CheckBox01)).isChecked()));
 				//debug(name.length()+"");
 			}
-			
+			//removeDialog(DIALOG_NEW);
+			//showItemList();			
 			//control
 			switch(button){
 			case R.id.Button01:case R.id.Button02:
-				this.findViewById(R.id.EditText01).clearFocus();
-				displayItemList();
+				//this.findViewById(R.id.EditText01).clearFocus();
+				dismissDialog(DIALOG_NEW);
+				/*removeDialog(DIALOG_NEW);*/
+				showItemList();
 				break;
 			case R.id.Button03:
-				displayNewItem();
+				//displayNewItem();
 				if(name.length()>=1)
-					((EditText) this.findViewById(R.id.EditText01)).setHint("Added " + name + ". Enter Next.");
+					hint = "Added " + name + ". Enter Next.";
+				showNewItem();
 				break; 
 			}
-			break;
-			
-			
-		case R.layout.shoppinglist:
-			switch(button){
-			case R.id.Button01:displayItemList();break;
-			//case R.id.Button02:displayNewItem();break;
-			}
-			break;
-		}		
+			break;			
+		
+		}	
 	}
 	
 	@Override
@@ -224,9 +300,42 @@ public class shopper extends Activity implements OnClickListener {
 	    	dialog.setContentView(R.layout.help);
 	    	dialog.setTitle(R.string.help);
 	        break;
+	    case DIALOG_NEW:
+	    	displayNewItem(dialog);    	
+	        break;
 	    default:
 	        dialog = null;
 	    }
 	    return dialog;
+	}
+
+	@Override
+	public void onTabChanged(String tabId) {
+		if(tabId == itemlist)
+			displayItemList();
+		else if(tabId == shoppingList)
+			displayShoppingList();
+		//shopper.debug(tabId);
+	}
+
+	@Override
+	public View createTabContent(String tag) {
+		LayoutInflater inflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		if(tag == itemlist)
+			return inflater.inflate(R.layout.main, null);
+		else if(tag == shoppingList)
+			return inflater.inflate(R.layout.shoppinglist, null);
+		return null;
+	}
+
+	@Override
+	protected void onPrepareDialog(int id, Dialog dialog) {
+		// TODO Auto-generated method stub
+		debug("Prepare dialog " +id);
+		if(id == DIALOG_NEW){
+			layout = R.layout.newitem;
+			displayNewItem(dialog);
+		}
+		super.onPrepareDialog(id, dialog);		
 	}
 }
